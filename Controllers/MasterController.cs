0c328d7c5668600461c864de9d26a01bc58c804f
barat <barat@DESKTOP-0ITTD35>
2023-07-12 21:09:01 +0530

    Add project files.

---
+++ Controllers/MasterController.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+namespace WEBSITE.Controllers
+{
+    public class MasterController : Controller
+    {
+        private IConfiguration Configuration;
+        public MasterController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            return View();
+        }
+
+        public IActionResult Service()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Contact(string name, string email, string subject, string message)
+        {
+            if(name != "" && name != null)
+            {
+                if (email != "" && email != null)
+                {
+                    if (subject != "" && subject != null)
+                    {
+                        if (message != "" && message != null)
+                        {
+                            StringBuilder sb = new StringBuilder();
+
+                            sb.AppendLine("<html>");
+                            sb.AppendLine("<body>");
+                            sb.AppendLine(@"<table style='padding: 10px; font-family: Verdana; font-size:11px;
+                        border-style:solid;border-width:1px;border-color:grey;'> ");
+
+                            sb.AppendLine("<tr>");
+                            sb.AppendLine("<td style='font-weight:bold;background-color:black;color:white;'>" + "User Name" + "</td>");
+                            sb.AppendLine("<td style='font-weight:bold;background-color:black;color:white;'>" + "User MailId" + "</td>");
+                            sb.AppendLine("</tr>");
+
+                            sb.AppendLine("<tr>");
+                            sb.AppendLine("<td style='text-align;left;'>" + name.ToString() + "</td>");
+                            sb.AppendLine("<td style='text-align;left;'>" + email.ToString() + "</td>");
+                            sb.AppendLine("</tr>");
+
+                            sb.AppendLine("</table>");
+
+                            sb.AppendLine("<br>");
+                            sb.AppendLine("<tr>");
+                            sb.AppendLine("<td style='text-align;left;'>" + message.ToString() + "</td>");
+                            sb.AppendLine("</tr>");
+
+
+                            sb.AppendLine("</body>");
+                            sb.AppendLine("</html>");
+
+                            string host = this.Configuration.GetValue<string>("Smtp:Server");
+                            int port = this.Configuration.GetValue<int>("Smtp:Port");
+                            string fromAddress = this.Configuration.GetValue<string>("Smtp:FromAddress");
+                            string userName = this.Configuration.GetValue<string>("Smtp:UserName");
+                            string password = this.Configuration.GetValue<string>("Smtp:Password");
+                            string ToAddress = this.Configuration.GetValue<string>("Smtp:ToAddress");
+
+                            using (MailMessage mm = new MailMessage())
+                            {
+                                mm.From = new MailAddress(fromAddress);
+                                mm.To.Add(ToAddress);
+                                mm.Subject = subject;
+                                mm.Body = sb.ToString();
+                                mm.IsBodyHtml = true;
+                                using (SmtpClient smtp = new SmtpClient())
+                                {
+                                    smtp.Host = host;
+                                    smtp.EnableSsl = true;
+                                    NetworkCredential NetworkCred = new NetworkCredential(userName, password);
+                                    smtp.UseDefaultCredentials = false;
+                                    smtp.Credentials = NetworkCred;
+                                    smtp.Port = port;
+                                    smtp.Send(mm);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            ViewBag.Message = string.Format("Message cannot be empty");
+
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.Message = string.Format("Subject cannot be empty");
+
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = string.Format("Email cannot be empty");
+                    return View();
+                }
+            }
+
+            else
+            {
+                ViewBag.Message = string.Format("Full Name cannot be empty");
+                return View();
+            }
+            
+            
+
+            return View();
+        }
+
+    }
+
+
+    //public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    //{
+    //    MailMessage message = new MailMessage();
+    //    message.Subject = subject;
+    //    message.Body = htmlMessage;
+    //    message.IsBodyHtml = true;
+    //    message.To.Add(email);
+
+    //    string host = _config.GetValue<string>("Smtp:Server", "defaultmailserver");
+    //    int port = _config.GetValue<int>("Smtp:Port", 25);
+    //    string fromAddress = _config.GetValue<string>("Smtp:FromAddress", "defaultfromaddress");
+
+    //    message.From = new MailAddress(fromAddress);
+
+    //    using (var smtpClient = new SmtpClient(host, port))
+    //    {
+    //        await smtpClient.SendMailAsync(message);
+    //    }
+    //}
+
+
+
+}
---
+++ Controllers/SendMail.cs
@@ -0,0 +1,157 @@
+﻿//using System;
+//using System.Data;
+//using System.Configuration;
+//using System.Linq;
+//using System.Web;
+//using System.Xml.Linq;
+//using DnsClient;
+
+//namespace WEBSITE.Controllers
+//{
+//    public class SendMail
+//    {
+//        public string FromName { get; set; }
+//        public string FromEmail { get; set; }
+//        public string ReplyToEmail { get; set; }
+//        public string ReplyToName { get; set; }
+
+//        public string Subject { get; set; }
+//        public string[] FileName { get; set; }
+//        public string Body { get; set; }
+
+//        public string ToName { get; set; }
+//        public string CcName { get; set; }
+//        public string BccName { get; set; }
+
+//        public string ToEmail { get; set; }
+//        public string CcEmail { get; set; }
+//        public string BccEmail { get; set; }
+//        public bool HtmlFormat { get; set; }
+//        public bool IsFileMemoryStream { get; set; }
+
+
+
+//        public SendMail()
+//        {
+           
+//        }
+
+//        public string send(string Company = "")
+//        {
+//            //Logging.logData mylog = new Logging.logData();
+
+//            try
+//            {
+//                string smtpServer = ConfigurationManager.AppSettings["smtpServer"];
+//                string mUser = _config.GetValue<string>("Smtp:Server", "defaultmailserver");
+//                string mPassword = ConfigurationManager.AppSettings["defaultMailPass"];
+//                string portNo = ConfigurationManager.AppSettings["smtpPort"];
+//                string isSSL = ConfigurationManager.AppSettings["defaultMailSSL"];
+
+//                if (FromEmail == null) FromEmail = ConfigurationManager.AppSettings["defaultMail"];
+//                if (portNo == "")
+//                    portNo = "25";
+
+//                bool SSLEnabled = false;
+//                bool.TryParse(isSSL, out SSLEnabled);
+
+//                //single dimension array
+//                ToName = "";
+//                CcName = "";
+//                BccName = "";
+
+               
+
+//                System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient(smtpServer);
+//                System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage();
+
+//                msg.IsBodyHtml = HtmlFormat;
+//                msg.Subject = Subject;
+//                msg.Body = Body;
+
+//                if (FileName != null)
+//                {
+//                    foreach (string str in FileName)
+//                    {
+                        
+//                        if (str.ToString() != "")
+//                        {
+//                            Console.WriteLine("File attached - " + str);
+//                            msg.Attachments.Add(new System.Net.Mail.Attachment(str.ToString()));
+//                        }
+//                    }
+//                }
+                
+//                msg.From = new System.Net.Mail.MailAddress(FromEmail, FromName);
+
+//                #region ToMails
+//                if (ToEmail != "" && !string.IsNullOrEmpty(ToEmail))
+//                {
+//                    string[] recipientsTo = ToEmail.Split(',');
+//                    if (recipientsTo.Length > 0)
+//                    {
+//                        for (int i = 0; i <= recipientsTo.Length - 1; i++)
+//                        {
+//                            if (recipientsTo[i].ToString().Length > 0)
+//                                msg.To.Add(new System.Net.Mail.MailAddress(recipientsTo[i].ToString(), ToName));
+//                        }
+//                    }
+//                    else
+//                    {
+//                        msg.To.Add(new System.Net.Mail.MailAddress(ToEmail, ToName));
+//                    }
+//                }
+//                #endregion
+
+//                #region CcMails
+//                if (CcEmail != "" && !string.IsNullOrEmpty(CcEmail))
+//                {
+//                    string[] recipientsCc = CcEmail.Split(',');
+//                    if (recipientsCc.Length > 0)
+//                    {
+//                        for (int i = 0; i <= recipientsCc.Length - 1; i++)
+//                            msg.CC.Add(new System.Net.Mail.MailAddress(recipientsCc[i].ToString(), ToName));
+//                    }
+//                    else
+//                    {
+//                        msg.CC.Add(new System.Net.Mail.MailAddress(CcEmail, CcName));
+//                    }
+//                }
+//                #endregion
+
+//                #region BccMails
+//                if (BccEmail != "" && !string.IsNullOrEmpty(BccEmail))
+//                {
+//                    string[] recipientsBcc = BccEmail.Split(',');
+//                    if (recipientsBcc.Length > 0)
+//                    {
+//                        for (int i = 0; i <= recipientsBcc.Length - 1; i++)
+//                            msg.Bcc.Add(new System.Net.Mail.MailAddress(recipientsBcc[i].ToString(), ToName));
+//                    }
+//                    else
+//                    {
+//                        msg.Bcc.Add(new System.Net.Mail.MailAddress(BccEmail, BccName));
+//                    }
+//                }
+//                #endregion
+
+//                System.Net.NetworkCredential basicAuthenticationInfo = new System.Net.NetworkCredential(mUser, mPassword);
+//                smtp.Port = int.Parse(portNo);
+//                smtp.Credentials = basicAuthenticationInfo;
+//                smtp.EnableSsl = SSLEnabled;
+//                smtp.Send(msg);
+//                msg = null;
+//                smtp = null;
+//                return "Mail sent.";
+//            }
+//            catch (Exception ex)
+//            {
+//                //mylog.LogTxt("", "sendMail", "", ex, "ReportConsole", "SYSTEM");
+//                return ex.ToString();
+//            }
+//        }
+
+//    }
+//}
+
+
